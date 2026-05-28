8c84185b2d390dd3ab0de5a14a5f621cab9ab634
Beatle <notbeatleog@gmail.com>
2026-05-28 22:19:55 +0530

    Change yt-dlp-exec to youtube-dl-exec

--- Backend/server.js
+++ Backend/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const path = require('path');
 const fs = require('fs');
-const ytDlp = require('yt-dlp-exec');
+const ytDlp = require('youtube-dl-exec');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
